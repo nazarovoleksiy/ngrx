1f17474a28b542e6d42de61037ac35f7e740ce5c
Nazarov Oleksiy <112445970+nazarovoleksiy@users.noreply.github.com>
2023-05-22 12:33:58 +0300

    NgRx first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CarsFormComponent } from './cars-form/cars-form.component';
+import { CarComponent } from './car/car.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CarsFormComponent,
+    CarComponent
   ],
   imports: [
     BrowserModule
